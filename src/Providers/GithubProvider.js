7cb41c1358b62b556d3d3f1b64b2f9e3c9b10fff
Bruno Felipe <oliveiraf.120@gmail.com>
2022-02-23 10:22:05 -0400

    feat: search if has a username in form. and add isLoading state to context

--- src/Providers/GithubProvider.js
+++ src/Providers/GithubProvider.js
@@ -9,12 +9,18 @@ export function GithubProvider({ children }) {
     repositories: [],
     starredRepositories: [],
   });
+  const [isLoading, setIsLoading] = useState(false);
 
   async function searchGithubUser(username) {
+    if (isLoading) return;
+    if (!username) return;
+
+    setIsLoading(true);
     const user = await api.getUser(username);
     const repositories = await api.getRepositories(username);
     const starredRepositories = await api.getStarredRepositories(username);
 
+    setIsLoading(false);
     setGithubData({
       user,
       repositories,
@@ -27,6 +33,7 @@ export function GithubProvider({ children }) {
       value={{
         githubData,
         searchGithubUser,
+        isLoading,
       }}
     >
       {children}
